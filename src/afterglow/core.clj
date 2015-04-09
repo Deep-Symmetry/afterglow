5b55446a04946208a8d3f099ca8c6accba891304
James Elliott <james.elliott@singlewire.com>
2015-04-09 00:22:41 -0500

    Testing setting DMX values.

--- src/afterglow/core.clj
+++ src/afterglow/core.clj
@@ -2,7 +2,9 @@
   (:require [flatland.protobuf.core :refer :all]
             [afterglow.ola-client :as ola]
             [taoensso.timbre :as timbre])
-  (:import [ola.proto Ola$PluginListRequest Ola$PluginListReply Ola$OptionalUniverseRequest Ola$UniverseInfoReply])
+  (:import [ola.proto Ola$PluginListRequest Ola$PluginListReply Ola$OptionalUniverseRequest Ola$UniverseInfoReply
+            Ola$DmxData Ola$Ack]
+           [com.google.protobuf ByteString])
   (:gen-class))
 
 (timbre/refer-timbre)
@@ -11,11 +13,16 @@
 (def PluginListReply (protodef Ola$PluginListReply))
 (def OptionalUniverseRequest (protodef Ola$OptionalUniverseRequest))
 (def UniverseInfoReply (protodef Ola$UniverseInfoReply))
+(def DmxData (protodef Ola$DmxData))
+(def Ack (protodef Ola$Ack))
 
 (defn -main
   "Test connectivity to the OLA server by sending a few messages and pretty-printing the responses."
   [& args]
-  (let [p (protobuf PluginListRequest)]
+  #_(let [p (protobuf PluginListRequest)]
     (ola/send-request "GetPlugins" p PluginListReply #(info "Plugins" (with-out-str (clojure.pprint/pprint %)))))
-  (let [p (protobuf OptionalUniverseRequest)]
-    (ola/send-request "GetUniverseInfo" p UniverseInfoReply #(info "Universes" (with-out-str (clojure.pprint/pprint %))))))
+  #_(let [p (protobuf OptionalUniverseRequest)]
+    (ola/send-request "GetUniverseInfo" p UniverseInfoReply #(info "Universes" (with-out-str (clojure.pprint/pprint %)))))
+  ;; TODO Build DMX values in a ByteBuffer, keeping track of max one set, then copy to ByteString and send to OLA. Profit!
+  (let [p (protobuf DmxData :universe 2 :data (ByteString/copyFrom (byte-array [3 2 1 52])))]
+    (ola/send-request "UpdateDmxData" p Ack #(info "Updated DMX Data" (with-out-str (clojure.pprint/pprint %))))))
