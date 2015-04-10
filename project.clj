c004c0d0436d0b0d3e106ad618f4a18e8658d197
James Elliott <james.elliott@singlewire.com>
2015-04-10 18:46:46 -0500

    Fix homepage now that there is a GitHub home.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject afterglow "0.1.0-SNAPSHOT"
   :description "A functional lighting controller working with Overtone and the Open Lighting Architecture"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/brunchboy/afterglow"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
