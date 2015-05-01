596656f28d9bf065a489b0bbe65246b1ee1252c0
James Elliott <james.elliott@singlewire.com>
2015-04-30 22:45:51 -0500

    Add ability to mix visible component of UV channel

--- src/afterglow/fixtures/chauvet.clj
+++ src/afterglow/fixtures/chauvet.clj
@@ -7,7 +7,7 @@
 
 (defn slimpar-hex3-irc
   ([]
-   (slimpar-hex3-irc :12-channel))
+   (slimpar-hex3-irc :12-channel-mix-uv))
   ([mode]
    (assoc (case mode
             ;; TODO missing channels once we have definition support for them
@@ -16,6 +16,12 @@
             :8-channel {:channels [(chan/dimmer 1) (chan/color 2 :red) (chan/color 3 :green) (chan/color 4 :blue)
                                    (chan/color 5 :amber :hue 45) (chan/color 6 :white) (chan/color 7 :uv :label "UV")]}
             :6-channel {:channels [(chan/color 1 :red) (chan/color 2 :green) (chan/color 3 :blue)
-                                   (chan/color 4 :amber :hue 45) (chan/color 5 :white) (chan/color 6 :uv :label "UV")]})
+                                   (chan/color 4 :amber :hue 45) (chan/color 5 :white) (chan/color 6 :uv :label "UV")]}
+            :12-channel-mix-uv {:channels [(chan/dimmer 1) (chan/color 2 :red) (chan/color 3 :green) (chan/color 4 :blue)
+                                    (chan/color 5 :amber :hue 45) (chan/color 6 :white) (chan/color 7 :uv :label "UV" :hue 270)]}
+            :8-channel-mix-uv {:channels [(chan/dimmer 1) (chan/color 2 :red) (chan/color 3 :green) (chan/color 4 :blue)
+                                   (chan/color 5 :amber :hue 45) (chan/color 6 :white) (chan/color 7 :uv :label "UV" :hue 270)]}
+            :6-channel-mix-uv {:channels [(chan/color 1 :red) (chan/color 2 :green) (chan/color 3 :blue)
+                                   (chan/color 4 :amber :hue 45) (chan/color 5 :white) (chan/color 6 :uv :label "UV" :hue 270)]})
           :name "Chauvet SlimPAR Hex 3 IRC"
           :mode mode)))
