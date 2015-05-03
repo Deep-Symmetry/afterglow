b955765e4bc5cce4e659019e6152b679a8f0e074
James Elliott <james.elliott@singlewire.com>
2015-05-03 18:15:38 -0500

    Make mixing of UV and amber keyword arguments.

--- src/afterglow/fixtures/chauvet.clj
+++ src/afterglow/fixtures/chauvet.clj
@@ -3,26 +3,22 @@
   (:require [afterglow.channels :as chan]))
 
 ;; TODO functions for rotational tranformatons
-;; TODO multi-head support, with relative locations
 ;; TODO make mixing of amber and UV keyword flags in the constructor, defaulting to on
 
 (defn slimpar-hex3-irc
   ([]
-   (slimpar-hex3-irc :12-channel-mix-uv))
-  ([mode]
+   (slimpar-hex3-irc :12-channel))
+  ([mode & {:keys [mix-amber mix-uv] :or {mix-amber true mix-uv true}}]
    (assoc (case mode
             ;; TODO missing channels once we have definition support for them
             :12-channel {:channels [(chan/dimmer 1) (chan/color 2 :red) (chan/color 3 :green) (chan/color 4 :blue)
-                                    (chan/color 5 :amber :hue 45) (chan/color 6 :white) (chan/color 7 :uv :label "UV")]}
+                                    (chan/color 5 :amber :hue (when mix-amber 45)) (chan/color 6 :white)
+                                    (chan/color 7 :uv :label "UV" :hue (when mix-uv 270))]}
             :8-channel {:channels [(chan/dimmer 1) (chan/color 2 :red) (chan/color 3 :green) (chan/color 4 :blue)
-                                   (chan/color 5 :amber :hue 45) (chan/color 6 :white) (chan/color 7 :uv :label "UV")]}
+                                   (chan/color 5 :amber :hue (when mix-amber 45)) (chan/color 6 :white)
+                                   (chan/color 7 :uv :label "UV" :hue (when mix-uv 270))]}
             :6-channel {:channels [(chan/color 1 :red) (chan/color 2 :green) (chan/color 3 :blue)
-                                   (chan/color 4 :amber :hue 45) (chan/color 5 :white) (chan/color 6 :uv :label "UV")]}
-            :12-channel-mix-uv {:channels [(chan/dimmer 1) (chan/color 2 :red) (chan/color 3 :green) (chan/color 4 :blue)
-                                    (chan/color 5 :amber :hue 45) (chan/color 6 :white) (chan/color 7 :uv :label "UV" :hue 270)]}
-            :8-channel-mix-uv {:channels [(chan/dimmer 1) (chan/color 2 :red) (chan/color 3 :green) (chan/color 4 :blue)
-                                   (chan/color 5 :amber :hue 45) (chan/color 6 :white) (chan/color 7 :uv :label "UV" :hue 270)]}
-            :6-channel-mix-uv {:channels [(chan/color 1 :red) (chan/color 2 :green) (chan/color 3 :blue)
-                                   (chan/color 4 :amber :hue 45) (chan/color 5 :white) (chan/color 6 :uv :label "UV" :hue 270)]})
+                                   (chan/color 4 :amber :hue (when mix-amber 45)) (chan/color 5 :white)
+                                   (chan/color 6 :uv :label "UV" :hue (when mix-uv 270))]})
           :name "Chauvet SlimPAR Hex 3 IRC"
           :mode mode)))
