1daa27fcba1b6b5ecb0bf542a06bb954da269b08
James Elliott <james.elliott@singlewire.com>
2015-05-23 23:28:15 -0500

    Add blade axis image.

--- src/afterglow/fixtures/blizzard.clj
+++ src/afterglow/fixtures/blizzard.clj
@@ -7,9 +7,36 @@
 ;; TODO functions for rotational tranformatons
 
 (defn blade-rgbw
-  "[Blade RGBW](http://www.blizzardlighting.com/index.php?option=com_k2&view=item&layout=item&id=177&Itemid=157)
-  moving head. The default mounting orientation is sitting on its feet with the LCD right side up and facing away
-  from the audience. In this orientation, at a pan of 0, the beam is straight into the audience."
+  "[Blade
+  RGBW](http://www.blizzardlighting.com/index.php?option=com_k2&view=item&layout=item&id=177&Itemid=157)
+  moving head. The default mounting orientation is sitting on its feet
+  with the LCD and control panel right side up and facing away from
+  the audience. In this orientation, at a pan of 0, the beam is nearly
+  straight into the audience.
+
+  The origin of the light is, as for all moving heads, at the
+  intersection of the pan and tilt axes. The image below shows this
+  default orientation, and the axes, for the fixture. If it is hung
+  with this side of the base facing the audience, right side up, then
+  you do not need to specify any rotations when you patch it.
+  Otherwise, tell afterglow how far it has been rotated around each of
+  the axes when hanging:
+
+  ![Blade
+  axes](https://raw.githubusercontent.com/brunchboy/afterglow/master/doc/assets/blade.png)
+
+  The center pan value (aimed straight at the audience when hung in
+  the default orientation described above), is defined as 85, a full
+  revolution around from that, so that it has room to move in both
+  directions from its resting point. It takes a change of +85 in the
+  pan channel to rotate a half circle counterclockwise around the Y
+  axis.
+
+  At the center pan setting of 85, the center tilt value is 8, aiming
+  the head straight out at the audience. At this position, it takes a
+  change of -214 in the tilt channel to rotate a half circle
+  counterclockwise around the X axis. (In other words, it can
+  essentially only tilt clockwise from here."
   {:doc/format :markdown}
   ([]
    (blade-rgbw :15-channel))
