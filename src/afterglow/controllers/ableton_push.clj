cf843a89ccbc253ef7913b9cf79f77317c270eeb
James Elliott <james.elliott@singlewire.com>
2015-06-01 10:56:14 -0500

    Add palette link, improve docs.

--- src/afterglow/controllers/ableton_push.clj
+++ src/afterglow/controllers/ableton_push.clj
@@ -1,6 +1,8 @@
 (ns afterglow.controllers.ableton-push
   "Allows the Ableton Push to be used as a control surface for
   Afterglow."
+  {:author "James Elliott"
+   :doc/format :markdown}
   (:require [clojure.math.numeric-tower :as math]
             [com.evocomputing.colors :as colors]
             [overtone.midi :as midi]))
@@ -16,27 +18,33 @@
 (defn velocity-for-color
   "Given a target color, calculate the MIDI note velocity which will
   achieve the closest approximation available on an Ableton Push
-  pad."
+  pad, using the thoughtful hue palette provided by Ableton:
+
+  ![Push pad palette](http://deepsymmetry.org/afterglow/research/PushColors.jpg)"
+  {:doc/format :markdown}
   [color]
   {:pre [(= (type color) :com.evocomputing.colors/color)]}
-  ;; Each hue section, starts with a desaturated version,
-  ;; then does a bright, medium, and dim version.
+  ;; Each hue section starts with a lightened version
+  ;; of the hue, then a bright, medium, and dim version
+  ;; of the fully-saturated hue.
   (let [brightness-shift (condp < (colors/lightness color)
                            60 0
                            37 1
                            15 2
                            3)]
-    (cond (< (colors/lightness color) 3)  ;; Black, off
+    (cond (< (colors/lightness color) 3)
+          ;; The color is effectively black.
           0
 
-          ;; The first four velocities are a white/gray section, starting
-          ;; with black as zero, through white as three.
+          ;; The color is effectively gray, so map it to the grayscale
+          ;; section, which ranges from black at zero, through white as
+          ;; three.
           (< (colors/saturation color) 20)
           (min 3 (- 4 brightness-shift))
 
           ;; Find the note value that approximates the hue and lightness.
           ;; From note 4 to note 59, the pads are in groups of four for
-          ;; a single hue, starting with the desaturated version, then
+          ;; a single hue, starting with the lightened version, then
           ;; the bright, medium, and dim versions.
           :else
           (let [hue-section (+ 4 (* 4 (math/floor (* 13 (/ (colors/hue color) 360)))))]
