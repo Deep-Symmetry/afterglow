1ec67cec102d05fb45c41d495814c55ae768ea29
James Elliott <james.elliott@singlewire.com>
2015-06-01 09:28:18 -0500

    Factor out color, fix coordinate calculation.

--- src/afterglow/controllers/ableton_push.clj
+++ src/afterglow/controllers/ableton_push.clj
@@ -7,35 +7,40 @@
 
 (defonce user-port-out (midi/midi-out "Ableton Push User Port"))
 
-(defn set-pad-color
-  "Set the color of one of the 64 touch pads to the closest
-  approximation available for a desired color."
-  [x y color]
-  {:pre [(<= 0 x 7) (<= 0 y 7) (= (type color) :com.evocomputing.colors/color)]}
-  (let [note (+ 36 (* x y))  ;; Note from grid coordinates
-        ;; For hues, starts with a desaturated version,
-        ;; then does a bright, medium, and dim version.
-        brightness-shift (condp < (colors/lightness color)
+(defn velocity-for-color
+  "Given a target color, calculate the MIDI note velocity which will
+  achieve the closest approximation available on an Ableton Push
+  pad."
+  [color]
+  {:pre [(= (type color) :com.evocomputing.colors/color)]}
+  ;; Each hue section, starts with a desaturated version,
+  ;; then does a bright, medium, and dim version.
+  (let [brightness-shift (condp < (colors/lightness color)
                            60 0
                            37 1
                            15 2
                            3)]
-    (println "lightness" (colors/lightness color) "brightness-shift" brightness-shift)
     (cond (< (colors/lightness color) 3)  ;; Black, off
-          (midi/midi-note-off user-port-out note)
+          0
 
           ;; The first four velocities are a white/gray section, starting
           ;; with black as zero, through white as three.
           (< (colors/saturation color) 20)
-          (midi/midi-note-on user-port-out note
-                             (min 3 (- 4 brightness-shift)))
+          (min 3 (- 4 brightness-shift))
 
           ;; Find the note value that approximates the hue and lightness.
           ;; From note 4 to note 59, the pads are in groups of four for
-          ;; a single hue, startind with the desaturated version, then
+          ;; a single hue, starting with the desaturated version, then
           ;; the bright, medium, and dim versions.
           :else
           (let [hue-section (+ 4 (* 4 (math/floor (* 13 (/ (colors/hue color) 360)))))]
-            (println "hue section" hue-section)
-            (midi/midi-note-on user-port-out note (+ hue-section brightness-shift))))))
+            (+ hue-section brightness-shift)))))
+
+(defn set-pad-color
+  "Set the color of one of the 64 touch pads to the closest
+  approximation available for a desired color."
+  [x y color]
+  {:pre [(<= 0 x 7) (<= 0 y 7)]}
+  (let [note (+ 36 x (* y 8))]  ;; Calculate note from grid coordinates
+    (midi/midi-note-on user-port-out note (velocity-for-color color))))
 
