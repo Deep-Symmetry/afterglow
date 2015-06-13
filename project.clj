f73ce5b9714387a77642d0620f4779f80a97bba1
James Elliott <james.elliott@singlewire.com>
2015-06-13 12:40:16 -0500

    Update timbre dependency.

--- project.clj
+++ project.clj
@@ -16,7 +16,7 @@
                  [selmer "0.8.2"]
                  [org.clojars.brunchboy/colors "1.0.2-SNAPSHOT"]
                  [environ "1.0.0"]
-                 [com.taoensso/timbre "4.0.0"]
+                 [com.taoensso/timbre "4.0.1"]
                  [com.taoensso/tower "3.0.2"]
                  [markdown-clj "0.9.66"]
                  [environ "1.0.0"]
