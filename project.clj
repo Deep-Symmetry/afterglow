cce81372a2cc0738f8111a9a78aac75abb4e95f1
James Elliott <james.elliott@singlewire.com>
2015-06-20 11:30:32 -0500

    Update Clojure version.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/brunchboy/afterglow"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
+  :dependencies [[org.clojure/clojure "1.7.0-RC2"]
                  [org.clojure/core.cache "0.6.4"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [org.clojure/math.numeric-tower "0.0.4"]
