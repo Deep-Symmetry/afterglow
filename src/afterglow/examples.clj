da3705b1d12854f3a3b08e2a4887b080d4a53bd3
James Elliott <james.elliott@singlewire.com>
2015-07-01 17:25:51 -0500

    Working on show arrangement for demo.

--- src/afterglow/examples.clj
+++ src/afterglow/examples.clj
@@ -45,33 +45,35 @@
   ;; Throw a couple of fixtures in there to play with. For better fun, use
   ;; fixtures and addresses that correspond to your actual hardware.
   (show/patch-fixture! :torrent-1 (blizzard/torrent-f3) universe 1
-                       :x (tf/inches 49) :y (tf/inches 61.5) :z (tf/inches 6)
-                       :y-rotation (tf/degrees -45))
+                       :x (tf/inches 44) :y (tf/inches 51.75) :z (tf/inches -4.75)
+                       :y-rotation (tf/degrees 0))
   (show/patch-fixture! :torrent-2 (blizzard/torrent-f3) universe 17
-                       :x (tf/inches -88) :y (tf/inches 12) :z (tf/inches 64)
-                       :y-rotation (tf/degrees 180))
-  (show/patch-fixture! :hex-1 (chauvet/slimpar-hex3-irc) universe 129 :y (tf/inches 4) :z (tf/inches 10)
+                       :x (tf/inches -44) :y (tf/inches 51.75) :z (tf/inches -4.75)
+                       :y-rotation (tf/degrees 0))
+  (show/patch-fixture! :hex-1 (chauvet/slimpar-hex3-irc) universe 129 :x (tf/inches 14) :y (tf/inches 44)
+                       :z (tf/inches -4.75)
                        :x-rotation (tf/degrees 90))
-  (show/patch-fixture! :hex-2 (chauvet/slimpar-hex3-irc) universe 145 :y (tf/inches 4) :z (tf/inches 30)
+  (show/patch-fixture! :hex-2 (chauvet/slimpar-hex3-irc) universe 145 :x (tf/inches -14) :y (tf/inches 44)
+                       :z (tf/inches -4.75)
                        :x-rotation (tf/degrees 90))
   (show/patch-fixture! :blade-1 (blizzard/blade-rgbw) universe 225
-                       :x (tf/inches -88) :y (tf/inches 9) :z (tf/inches 66)
-                       :y-rotation (tf/degrees -180))
-  (show/patch-fixture! :blade-2 (blizzard/blade-rgbw) universe 240
-                       :x (tf/inches 40) :y (tf/inches 58) :z (tf/inches -15)
+                       :x (tf/inches 26.5) :y (tf/inches 48.5) :z (tf/inches -4.75)
                        :y-rotation (tf/degrees -45))
+  (show/patch-fixture! :blade-2 (blizzard/blade-rgbw) universe 240
+                       :x (tf/inches -26.5) :y (tf/inches 48.5) :z (tf/inches -4.75)
+                       :y-rotation (tf/degrees 45))
   (show/patch-fixture! :blade-3 (blizzard/blade-rgbw) universe 255
-                       :x (tf/inches -58) :y (tf/inches 9) :z (tf/inches 66)
-                       :y-rotation (tf/degrees 90))
-  (show/patch-fixture! :blade-4 (blizzard/blade-rgbw) universe 270 :y (tf/inches 9))
+                       :x (tf/inches 0) :y (tf/inches 38.75) :z (tf/inches 207.5)
+                       :y-rotation (tf/degrees 180))
+  (show/patch-fixture! :blade-4 (blizzard/blade-rgbw) universe 270 :y (tf/inches 48.5) :z (tf/inches -4.75))
   (show/patch-fixture! :ws-1 (blizzard/weather-system) universe 161
-                       :x (tf/inches 22) :y (tf/inches 7) :z (tf/inches 7)
-                       :x-rotation (tf/degrees 180) :y-rotation (tf/degrees 180))
+                       :x (tf/inches 55) :y (tf/inches 71) :z (tf/inches 261) :y-rotation (tf/degrees 225))
   (show/patch-fixture! :ws-2 (blizzard/weather-system) universe 187
-                       :x (tf/inches -76) :y (tf/inches 64) :z (tf/inches -4))
+                       :x (tf/inches -55) :y (tf/inches 71) :z (tf/inches 261) :y-rotation (tf/degrees 135))
   (show/patch-fixture! :puck-1 (blizzard/puck-fab5) universe 97 :x (tf/inches -76) :y (tf/inches 8) :z (tf/inches 52))
   (show/patch-fixture! :puck-2 (blizzard/puck-fab5) universe 113 :x (tf/inches -76) :y (tf/inches 8) :z (tf/inches 40))
-  (show/patch-fixture! :snowball (blizzard/snowball) universe 33 :x (tf/inches -76) :y (tf/inches 10) :z (tf/inches 60))
+  (show/patch-fixture! :snowball (blizzard/snowball) universe 33 :x (tf/inches -76) :y (tf/inches 32)
+                       :z (tf/inches 164.5))
   (show/patch-fixture! :hyp-rgb (adj/hypnotic-rgb) universe 45)
   '*show*)
 
