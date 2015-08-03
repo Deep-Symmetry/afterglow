eba385fe8e91a7d16798846879b6d80c90784828
James Elliott <james.elliott@singlewire.com>
2015-08-02 22:10:58 -0500

    Release 0.1.1 for afterglow-max
    Build and push a new release in preparation for the first release of the
    afterglow-max project that embeds it.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject afterglow "0.1.1-SNAPSHOT"
+(defproject afterglow "0.1.1"
   :description "A live-coding environment for light shows, built on the Open Lighting Architecture, using bits of Overtone."
   :url "https://github.com/brunchboy/afterglow"
   :license {:name "Eclipse Public License"
