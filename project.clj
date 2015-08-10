6a8d884fdc9c5487d9518d9823a8fa495a28c339
James Elliott <james.elliott@singlewire.com>
2015-08-09 22:54:48 -0500

    Open up for work on next release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject afterglow "0.1.2"
+(defproject afterglow "0.1.3-SNAPSHOT"
   :description "A live-coding environment for light shows, built on the Open Lighting Architecture, using bits of Overtone."
   :url "https://github.com/brunchboy/afterglow"
   :license {:name "Eclipse Public License"
