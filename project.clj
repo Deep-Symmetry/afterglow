bc52a0a78ef8c9307a1ef296acb20b714dd29ff9
James Elliott <james@deepsymmetry.org>
2016-06-06 22:19:29 -0500

    Update dependency.

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
                  [org.clojure/tools.cli "0.3.5"]
                  [org.clojure/tools.nrepl "0.2.12"]
                  [org.clojure/tools.reader "0.10.0"]
-                 [org.deepsymmetry/beat-link "0.1.6"]
+                 [org.deepsymmetry/beat-link "0.1.7"]
                  [org.deepsymmetry/wayang "0.1.5"]
                  [java3d/vecmath "1.3.1"]
                  [java3d/j3d-core "1.3.1"]
