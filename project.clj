0e216daf438d7b497615132efcb44c1e662ace33
James Elliott <james@deepsymmetry.org>
2016-08-31 22:46:57 -0500

    Update dependency.

--- project.clj
+++ project.clj
@@ -31,7 +31,7 @@
                  [com.evocomputing/colors "1.0.3"]
                  [environ "1.0.3"]
                  [camel-snake-kebab "0.4.0"]
-                 [com.taoensso/timbre "4.7.0"]
+                 [com.taoensso/timbre "4.7.4"]
                  [com.fzakaria/slf4j-timbre "0.3.2"]
                  [com.taoensso/tower "3.0.2"]
                  [com.taoensso/truss "1.3.3"]
