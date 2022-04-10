8bad63dc1b7f47f0fceb525f4a645b64c560436b
James Elliott <james@deepsymmetry.org>
2022-04-10 13:51:04 -0500

    Fix issue building on Java 16 and later
    Codox needs some packages opened until they can get some of their own
    dependencies updated or replaced.
    
    This commit also updates to the latest Clojure and versions of various
    dependencies.

--- project.clj
+++ project.clj
@@ -3,8 +3,9 @@
   :url "https://github.com/Deep-Symmetry/afterglow"
   :license {:name "Eclipse Public License 2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
-  :jvm-opts ["-Dapple.awt.UIElement=true"]  ; Suppress dock icon and focus stealing when compiling on a Mac.
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :jvm-opts ["--add-opens" "java.base/java.lang=ALL-UNNAMED"  ; Work around Codox issue on Java 16 and later.
+             "-Dapple.awt.UIElement=true"]  ; Suppress dock icon and focus stealing when compiling on a Mac.
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/core.cache "1.0.225"]
                  [org.clojure/core.async "1.5.648" :exclusions [org.clojure/tools.reader]]
                  [org.clojure/data.json "2.4.0"]
@@ -13,7 +14,7 @@
                  [org.clojure/tools.cli "1.0.206"]
                  [org.clojure/tools.nrepl "0.2.13"]
                  [org.clojure/tools.reader "1.3.6"]
-                 [org.deepsymmetry/beat-link "0.6.3" :exclusions [org.slf4j/slf4j-api]]
+                 [org.deepsymmetry/beat-link "7.0.0" :exclusions [org.slf4j/slf4j-api]]
                  [org.deepsymmetry/lib-carabiner "1.1.7-SNAPSHOT"]
                  [org.deepsymmetry/wayang "0.1.8"]
                  [java3d/vecmath "1.3.1"]
@@ -28,16 +29,16 @@
                  [com.climate/claypoole "1.1.4"]
                  [org.clojars.brunchboy/protobuf "0.8.3"]
                  [ola-clojure "0.1.8" :exclusions [org.clojure/tools.reader]]
-                 [selmer "1.12.49" :exclusions [cheshire]]
+                 [selmer "1.12.50" :exclusions [cheshire]]
                  [com.evocomputing/colors "1.0.6"]
                  [environ "1.2.0"]
                  [camel-snake-kebab "0.4.2"]
-                 [com.taoensso/timbre "5.1.2"]
+                 [com.taoensso/timbre "5.2.1"]
                  [com.taoensso/tufte "2.2.0"]
                  [com.fzakaria/slf4j-timbre "0.3.21"]
                  [com.taoensso/tower "3.0.2"]
                  [com.taoensso/truss "1.6.0"]
-                 [markdown-clj "1.10.8"]
+                 [markdown-clj "1.11.0"]
                  [ring/ring-core "1.9.5"]
                  [clj-time "0.15.2"]
                  [compojure "1.6.2" :exclusions [org.eclipse.jetty/jetty-server
@@ -45,7 +46,7 @@
                                                  ring/ring-codec]]
                  [ring/ring-defaults "0.3.3"]
                  [ring/ring-session-timeout "0.2.0"]
-                 [ring-middleware-format "0.7.4" :exclusions [ring/ring-jetty-adapter
+                 [ring-middleware-format "0.7.5" :exclusions [ring/ring-jetty-adapter
                                                               cheshire
                                                               org.clojure/tools.reader
                                                               org.clojure/java.classpath
